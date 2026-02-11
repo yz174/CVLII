0f9d629b6e9f120e4d5a65c2ac4b969166be8131
Ujjwal Atri <ujjwalkrish082@gmail.com>
2026-02-12 03:26:01 +0530

    Add __init__.py and __main__.py to make tui_resume package executable

---
+++ src/tui_resume/__init__.py
@@ -0,0 +1,4 @@
+"""TUI Resume - Interactive Terminal Portfolio Application"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
---
+++ src/tui_resume/__main__.py
@@ -0,0 +1,6 @@
+"""Make tui_resume package executable as a module"""
+
+from .app import main
+
+if __name__ == "__main__":
+    main()
