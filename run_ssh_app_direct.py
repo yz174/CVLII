20005e26a13488b45dd01663a24432016196f99c
Ujjwal Atri <ujjwalkrish082@gmail.com>
2026-02-12 22:08:49 +0530

    Add mouse support disabling for SSH in Textual app

--- run_ssh_app_direct.py
+++ run_ssh_app_direct.py
@@ -10,12 +10,17 @@ os.environ['PYTHONUNBUFFERED'] = '1'
 if 'TERM' not in os.environ:
     os.environ['TERM'] = 'xterm-256color'
 
+# Disable mouse support for SSH
+os.environ['TEXTUAL_NO_MOUSE'] = '1'
+
 # Import and run the app
 from src.tui_resume.app import ResumeApp
 
 if __name__ == "__main__":
     try:
         app = ResumeApp()
+        # Disable mouse support
+        app._disable_mouse = True
         app.run()
     except Exception as e:
         print(f"ERROR: {e}", file=sys.stderr, flush=True)
